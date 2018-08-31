3347a00072d1c4d3fe752e8fd1e5b8ef0d99b677
Manuel Diaz <manuel.ade@gmail.com>
2018-08-31 15:08:15 +0800

    Initial commit

--- CompareSolutionsWENO5LF.m
+++ CompareSolutionsWENO5LF.m
@@ -36,35 +36,37 @@ plot(x,r_FD_2,'-');
 plot(x,r_FV_1,'-');
 plot(x,r_FV_2,'-');
 plot(x,r_FV_3,'-'); hold off
-title('Expansion Fan'); 
+title('The Gas Dynamics Euler Equations (EE)'); 
 xlabel('$x$','interpreter','latex'); ylabel('$\varrho(x)$','interpreter','latex');
-legend({'Exact','FD-comp','FD-char','FV-comp','FV-prim','FV-char'},...
-    'interpreter','latex'); 
+legend({'Exact','FD Component-wise','FD Characteristic-wise','FV Component-wise','FV Primitive-wise','FV Characteristic-wise'},...
+    'interpreter','latex','location','southwest'); 
 legend boxoff
+fig=gcf; fig.PaperUnits='inches'; fig.PaperPosition=[0 0 6 4];
+print('-depsc','SodShockTube1d.eps');
 
 % Plot subfigures 
 figure(2)
 subplot(131)
 plot(xe,re,'-k'); hold on 
-plot(x,r_FD_1,'-');
-plot(x,r_FD_2,'-');
-plot(x,r_FV_1,'-');
-plot(x,r_FV_2,'-');
-plot(x,r_FV_3,'-'); hold off
+plot(x,r_FD_1,'-x');
+plot(x,r_FD_2,'-o');
+plot(x,r_FV_1,'-+');
+plot(x,r_FV_2,'-s');
+plot(x,r_FV_3,'-^'); hold off
 title('Expansion Fan'); 
 xlabel('$x$','interpreter','latex'); ylabel('$\varrho(x)$','interpreter','latex');
 legend({'Exact','FD-comp','FD-char','FV-comp','FV-prim','FV-char'},...
     'interpreter','latex'); 
 legend boxoff
-xlim([0.4,0.6]);
+xlim([0.5,0.65]);
 
 subplot(132)
 plot(xe,re,'-k'); hold on 
-plot(x,r_FD_1,'-');
-plot(x,r_FD_2,'-');
-plot(x,r_FV_1,'-');
-plot(x,r_FV_2,'-');
-plot(x,r_FV_3,'-'); hold off
+plot(x,r_FD_1,'-x');
+plot(x,r_FD_2,'-o');
+plot(x,r_FV_1,'-+');
+plot(x,r_FV_2,'-s');
+plot(x,r_FV_3,'-^'); hold off
 title('Contact'); 
 xlabel('$x$','interpreter','latex'); ylabel('$\varrho(x)$','interpreter','latex');
 legend({'Exact','FD-comp','FD-char','FV-comp','FV-prim','FV-char'},...
@@ -74,14 +76,16 @@ xlim([0.7,0.8]);
 
 subplot(133)
 plot(xe,re,'-k'); hold on 
-plot(x,r_FD_1,'-');
-plot(x,r_FD_2,'-');
-plot(x,r_FV_1,'-');
-plot(x,r_FV_2,'-');
-plot(x,r_FV_3,'-'); hold off
+plot(x,r_FD_1,'-x');
+plot(x,r_FD_2,'-o');
+plot(x,r_FV_1,'-+');
+plot(x,r_FV_2,'-s');
+plot(x,r_FV_3,'-^'); hold off
 title('Shock'); 
 xlabel('$x$','interpreter','latex'); ylabel('$\varrho(x)$','interpreter','latex');
 legend({'Exact','FD-comp','FD-char','FV-comp','FV-prim','FV-char'},...
     'interpreter','latex'); 
 legend boxoff
 xlim([0.85,0.9]);
+fig=gcf; fig.PaperUnits='inches'; fig.PaperPosition=[0 0 12 6];
+print('-depsc','SodShockTube1d_basicStructures.eps');
